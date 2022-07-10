e367ebbafa86856c7ea53cf19911321c2716d3ed
vishwa-21 <50892596+vishwa-21@users.noreply.github.com>
2022-07-09 18:25:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ def home():
 
 @app.route('/resource-catalog')
 def rCatalog():
-	return "Hi"
+	return render_template("rCatalog.html")
 
 @app.route("/connect")
 def connect():
-	return "Hi"
+	return render_template("connect.html");
+
+@app.route("/contribute")
+def contribute():
+	return render_template("contribute.html");
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
